77537a8c1ea1f17d8c61c4779f9929349dd39e75
Karan Verma <karanverma77420@gmail.com>
2024-12-06 23:18:30 +0530

    Initial Commit

---
+++ Swiggy_Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swiggy_Assignment
+//
+//  Created by Karan Verma on 06/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
